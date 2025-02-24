f47ef0e39e4ee684df81ebe447a37896709bf3f6
guruvignesh77 <gurudigitals88@gmail.com>
2025-02-24 10:13:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,13 @@ id_pattern = re.compile(r'^.\d+$')
 class Rkn_Bots(object):
     
     # Rkn client config  ( required.. 😥)
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = os.environ.get("API_ID", "
+20674016")
+    API_HASH = os.environ.get("API_HASH", "dd9fd170543f9ba5c191539674250740")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7828401886:AAGjJgD1e9KO8Kc-W8olgvnJKh4Cqj5T0iE")
 
     # start_pic
-    RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
+    RKN_PIC = os.environ.get("RKN_PIC", "https://graph.org/file/d4944726f657fde8e5178-6c8155880ca3dd3eb0.jpg")
 
     # wes response configuration
     BOT_UPTIME = time.time()
@@ -29,7 +30,7 @@ class Rkn_Bots(object):
     DB_URL = os.environ.get("DB_URL", "")
 
     # default caption 
-    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https//:t.me/Rkn_Botz'>{file_name} Main Telegram Channel: @RknDeveloper</a></b>",
+    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https//:t.me/Rkn_Botz'>{file_name} Main Telegram Channel: @Goatmoviebot</a></b>",
     )
 
     # sticker Id
